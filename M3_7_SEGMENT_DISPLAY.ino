f7c7af5a59bcb139fabcfc2a3f1f046b528f0677
zaeziezoe <gzordonez@gbox.adnu.edu.ph>
2025-06-20 10:26:54 +0800

    M3_7_SEGMENT_DISPLAY.ino

--- M3_7_SEGMENT_DISPLAY.ino
+++ M3_7_SEGMENT_DISPLAY.ino
@@ -1,39 +1,33 @@
-const int clockPin = 2;  // Shift register clock pin (SH_CP)
-const int latchPin = 3;  // Shift register latch pin (ST_CP)
-const int dataPin = 4;   // Shift register data pin (DS)
+const int clockPin = 2;    
+const int latchPin = 3;   
+const int dataPin = 4;   
 
+// 7-segment patterns for digits 0-9 (common cathode)  
+// Bits: DP-G-F-E-D-C-B-A 
+const byte digitPatterns[10] = {  
+  0x3F, // 0  
+  0x06, // 1  
+  0x5B, // 2  
+  0x4F, // 3  
+  0x66, // 4  
+  0x6D, // 5  
+  0x7D, // 6  
+  0x07, // 7  
+  0x7F, // 8  
+  0x6F  // 9  
+};  
 
-// 7-segment patterns for digits 0-9 (common cathode)
-// Bits represent: DP-G-F-E-D-C-B-A
-const byte digitPatterns[10] = {
-  0x3F, // 0: ABCDEF
-  0x06, // 1: BC
-  0x5B, // 2: ABDEG
-  0x4F, // 3: ABCDG
-  0x66, // 4: BCDF
-  0x6D, // 5: ACDFG
-  0x7D, // 6: ACDEFG
-  0x07, // 7: ABC
-  0x7F, // 8: ABCDEFG
-  0x6F  // 9: ABCDFG
-};
+void setup() {  
+  pinMode(clockPin, OUTPUT);  
+  pinMode(latchPin, OUTPUT);  
+  pinMode(dataPin, OUTPUT);  
+}  
 
-
-void setup() {
-  pinMode(clockPin, OUTPUT);
-  pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-}
-
-
-void loop() {
-  // Cycle through digits 0-9
-  for (int digit = 0; digit < 10; digit++) {
-    // Load data into shift register
-    digitalWrite(latchPin, LOW);              // Begin data transfer
-    shiftOut(dataPin, clockPin, MSBFIRST, digitPatterns[digit]);  // Send segment pattern
-    digitalWrite(latchPin, HIGH);             // Update output
-   
-    delay(500);  // Display each digit for 0.5 seconds
-  }
-}
+void loop() {  
+  for (int digit = 0; digit < 10; digit++) {  
+    digitalWrite(latchPin, LOW);  
+    shiftOut(dataPin, clockPin, MSBFIRST, digitPatterns[digit]);  
+    digitalWrite(latchPin, HIGH);  
+    delay(500);  // Adjust display time (milliseconds)  
+  }  
+} 
