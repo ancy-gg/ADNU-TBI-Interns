fa6d44062f2afde6669b00f2ad9f1a37e36dac42
zaeziezoe <gzordonez@gbox.adnu.edu.ph>
2025-06-20 10:22:27 +0800

    M7_DC_MOTOR.ino

--- M7_DC_MOTOR.ino
+++ M7_DC_MOTOR.ino
@@ -1,31 +1,24 @@
-//Clockwise & Counter-clockwise
-const int motorIn1 = 9;   // Motor control pin 1 (PWM capable)
-const int motorIn2 = 10;  // Motor control pin 2 (PWM capable)
+const int motorIn1 = 9;    
+const int motorIn2 = 10;   
 
+void setup() {  
+  pinMode(motorIn1, OUTPUT);  
+  pinMode(motorIn2, OUTPUT);  
+}  
 
-void setup() {
-  pinMode(motorIn1, OUTPUT);  // Set motorIn1 as output (controls voltage/speed)
-  pinMode(motorIn2, OUTPUT);  // Set motorIn2 as output (controls voltage/speed)
-}
+void loop() {  
+  clockwise(120);        // Adjust speed (0-255)  
+  delay(1000);           // Adjust delay (in milliseconds)  
+  counterclockwise(120); // Adjust speed (0-255)  
+  delay(1000);           // Adjust delay (in milliseconds)  
+}  
 
+void clockwise(int Speed) {  
+  analogWrite(motorIn1, Speed);  
+  analogWrite(motorIn2, 0);  
+}  
 
-void loop() {
-  clockwise(200);        // Rotate motor clockwise at PWM=120
-  delay(1000);           // Maintain rotation for 1 second
-  counterclockwise(200); // Rotate motor counter-clockwise at PWM=120
-  delay(1000);           // Maintain rotation for 1 second
-}
-
-
-// Rotates motor clockwise by energizing motorIn1
-void clockwise(int Speed) {
-  analogWrite(motorIn1, Speed);  // Apply PWM voltage to motorIn1 (0-255)
-  analogWrite(motorIn2, 0);      // Set motorIn2 to 0V (GND reference)
-}
-
-
-// Rotates motor counter-clockwise by energizing motorIn2
-void counterclockwise(int Speed) {
-  analogWrite(motorIn1, 0);      // Set motorIn1 to 0V (GND reference)
-  analogWrite(motorIn2, Speed);  // Apply PWM voltage to motorIn2 (0-255)
-}
+void counterclockwise(int Speed) {  
+  analogWrite(motorIn1, 0);  
+  analogWrite(motorIn2, Speed);  
+} 
